9aa55f890ed7348cd05076fcb2c4eb33ff838143
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2025-12-04 23:17:33 +0500

    day3 and day4

---
+++ src/bin/03.rs
@@ -0,0 +1,90 @@
+use adv_code_2025::*;
+use anyhow::*;
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use itertools::Itertools;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const DAY: &str = "03";
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+const TEST: &str = "\
+987654321111111
+811111111111119
+234234234234278
+818181911112111";
+
+fn solver<R: BufRead>(reader: R, times: usize) -> Result<usize> {
+    let answer = reader
+        .lines()
+        .map(|l| {
+            l.unwrap()
+                .split("")
+                .filter_map(|d| d.parse::<usize>().ok())
+                .enumerate()
+                .collect_vec()
+        })
+        .map(|nums| {
+            let mut result: Vec<usize> = vec![];
+
+            let mut left = 0;
+            let mut right = nums.len() - times + 1;
+
+            while result.len() < times && left < nums.len() {
+                let window = &nums[left..right];
+
+                let max = window.iter().max_set_by(|a, b| a.1.cmp(&b.1));
+                let (index, num) = max.first().unwrap();
+
+                result.push(*num);
+
+                left = index + 1;
+                if right < nums.len() {
+                    right += 1;
+                }
+            }
+
+            result
+                .into_iter()
+                .reduce(|acc, d| acc * 10 + d)
+                .unwrap_or(0)
+        })
+        .sum();
+
+    Ok(answer)
+}
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    //region Part 1
+    println!("=== Part 1 ===");
+
+    fn part1<R: BufRead>(reader: R) -> Result<usize> {
+        solver(reader, 2)
+    }
+
+    assert_eq!(357, part1(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {}", result);
+    //endregion
+
+    //region Part 2
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        solver(reader, 12)
+    }
+
+    assert_eq!(3121910778619, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
+    //endregion
+
+    Ok(())
+}
---
+++ src/bin/04.rs
@@ -0,0 +1,171 @@
+use adv_code_2025::*;
+use anyhow::*;
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use itertools::Itertools;
+use std::fmt::Debug;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::ops::DerefMut;
+
+const DAY: &str = "04";
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+const TEST: &str = "\
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.";
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Place {
+    Empty,
+    Paper,
+    Marked,
+    Removed,
+}
+
+impl Debug for Place {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Place::Empty => ".",
+                Place::Paper => "@",
+                Place::Marked => "#",
+                Place::Removed => "x",
+            }
+        )
+    }
+}
+
+impl Place {
+    fn parse(value: char) -> Place {
+        match value {
+            '@' => Place::Paper,
+            _ => Place::Empty,
+        }
+    }
+
+    fn is_marked(&self, x: usize, y: usize, map: &[Vec<Place>]) -> bool {
+        let ix = x as isize;
+        let iy = y as isize;
+
+        let adjasent = [
+            (ix - 1, iy - 1),
+            (ix, iy - 1),
+            (ix + 1, iy - 1),
+            (ix - 1, iy),
+            (ix + 1, iy),
+            (ix - 1, iy + 1),
+            (ix, iy + 1),
+            (ix + 1, iy + 1),
+        ];
+
+        let adjasent = adjasent
+            .iter()
+            .filter_map(|(x, y)| {
+                let x = usize::try_from(*x).ok()?;
+                let y = usize::try_from(*y).ok()?;
+                let place = map.get(y)?.get(x)?;
+                match place {
+                    Place::Paper => Some(()),
+                    _ => None,
+                }
+            })
+            .count();
+
+        adjasent < 4
+    }
+}
+
+fn solver<R: BufRead>(reader: R, once: bool) -> Result<usize> {
+    let mut answer = reader
+        .lines()
+        .map(|l| l.unwrap().chars().map(Place::parse).collect_vec())
+        .collect_vec();
+
+    let mut result = 0;
+    loop {
+        let mut marked_p = vec![];
+        let mut removed_p = vec![];
+
+        answer.iter().enumerate().for_each(|(y, row)| {
+            row.iter().enumerate().for_each(|(x, place)| match place {
+                Place::Empty | Place::Removed => (),
+                Place::Paper => {
+                    if place.is_marked(x, y, &answer) {
+                        marked_p.push((x, y));
+                    }
+                }
+                Place::Marked => {
+                    removed_p.push((x, y));
+                }
+            })
+        });
+
+        for (x, y) in &marked_p {
+            let place = answer.get_mut(*y).unwrap().get_mut(*x).unwrap();
+            *place = Place::Marked;
+        }
+
+        for (x, y) in &removed_p {
+            let place = answer.get_mut(*y).unwrap().get_mut(*x).unwrap();
+            *place = Place::Removed;
+        }
+
+        if marked_p.is_empty() {
+            break;
+        }
+
+        result += marked_p.len();
+
+        if once {
+            break;
+        }
+    }
+
+    Ok(result)
+}
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    //region Part 1
+    println!("=== Part 1 ===");
+
+    fn part1<R: BufRead>(reader: R) -> Result<usize> {
+        solver(reader, true)
+    }
+
+    // TODO: Set the expected answer for the test input
+    assert_eq!(13, part1(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {}", result);
+    //endregion
+
+    //region Part 2
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        solver(reader, false)
+    }
+
+    assert_eq!(43, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
+    //endregion
+
+    Ok(())
+}
